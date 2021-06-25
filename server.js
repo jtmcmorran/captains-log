21775b470aff6693ef77fc120cac7bbbf562ee5c
James McMorran <jtmcmorran@gmail.com>
2021-06-25 12:54:03 -0400

    created 'create' route

--- server.js
+++ server.js
@@ -8,19 +8,23 @@ const PORT = 3000;
 //view engine setup
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
-
+app.use(express.urlencoded({extended:true}));
 //index
 
 //new
 app.get('/logs/new',(req,res)=>{
-  res.render('New')
+  res.render('New');
 })
 //delete
 
 //update
 
 //create
-
+app.post('/logs',(req,res)=>{
+  if(req.body.shipIsBroken ==='on') req.body.shipIsBroken = true;
+  else req.body.shipIsBroken = false;
+  res.send(req.body);
+})
 //edit
 
 //show
